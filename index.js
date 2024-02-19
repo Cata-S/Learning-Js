70f01d8a55f64ba4be022df4bfae27af50970cb9
Cata <c.silav@yahoo.com>
2024-02-19 17:15:01 +0200

    variables/boolians practice

---
+++ index.js
@@ -0,0 +1,27 @@
+//console.log(`Hello`);
+//console.log(`I Like Pizza`);
+
+//window.alert(`This is an alert!`);
+
+document.getElementById("myH1").textContent = `Hello`;
+document.getElementById("myP").textContent = `I Like pizza!`;
+
+let age = 25;
+let price = 10.99;
+let height = 1.86;
+
+console.log(`You are ${age} years old`);
+console.log(`The price is $${price}`);
+console.log(`Your height is ${height} meters`);
+
+let firstName = "Bro";
+let favoriteFoood = "pizza";
+let email = "c.silav@yahoo.com";
+let forSale = true;
+let isStudent = true;
+
+console.log(`You are ${firstName}`);
+console.log(`You like ${favoriteFoood}`);
+console.log(`Your email is ${email}`);
+console.log(`Is it for sale? ${forSale}`);
+console.log(`Enrolled: ${isStudent}`);
